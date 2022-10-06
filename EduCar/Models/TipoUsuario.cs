79f91c36033f1974b204e7ee62f468c5fbbb63a7
Paloma Souza <paloma.souza112@gmail.com>
2022-10-06 20:47:20 -0300

    Models Cambio, CaracteristicasGerais, TipoUsuario e Veiculo criados

---
+++ EduCar/Models/TipoUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduCar.Models
+{
+    public class TipoUsuario
+    {
+       
+            // Na classe Model, haverá todos os atributos/colunas que compõe a classe TipoUsuario
+
+            [Key]   // primary key 
+            public int Id { get; set; }
+
+            [Required(ErrorMessage ="Tipo é obrigatório")]  // campo obrigatório
+            public string Tipo { get; set; }
+        }
+ 
+
+}
