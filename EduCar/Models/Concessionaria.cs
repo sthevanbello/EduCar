a4bbcb94becb3323a0c565d2e782f39b749c3b31
Fagner Santos <uprogramador@gmail.com>
2022-10-06 20:18:42 -0300

    Criação das Models Cartão, Concessionária, Direcão e Endereço

---
+++ EduCar/Models/Concessionaria.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduCar.Models
+{
+    public class Concessionaria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome da Concessionária é obrigatório")]        
+        public string Nome { get; set; }
+
+        [ForeignKey("Endereco")]
+        public int IdEndereco { get; set; }
+        public Endereco Endereco { get; set; }
+
+        //public ICollection<Veiculos> Veiculos{ get; set; }
+        public ICollection<Pedido> Pedido { get; set; }
+
+
+    }
+}
---
+++ EduCar/Models/Direcao.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduCar.Models
+{
+    public class Direcao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tipo é obrigatório")]
+        public string Tipo { get; set; }
+        public ICollection<FichaTecnica> FichaTecnica { get; set; }
+    }
+}
