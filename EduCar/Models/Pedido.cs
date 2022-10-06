e320cb9bd28894055d1aba31c3fe87c6e38119a1
Felipe Diniz <fdsampaio@yahoo.com.br>
2022-10-06 14:39:54 -0300

    feat: Criação do Model Pedido

---
+++ EduCar/Models/Pedido.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduCar.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //[ForeignKey("Usuario")]
+        //public int IdUsuario { get; set; }
+        //public Usuario Usuario { get; set; }
+
+        //[ForeignKey("Concessionaria")]
+        //public int IdConcessionaria { get; set; }
+        //public Concessionaria Concessionaria { get; set; }
+
+        //[ForeignKey("Veiculo")]
+        //public int IdVeiculo { get; set; }
+        //public Veiculo Veiculo { get; set; }
+
+
+
+    }
+}
