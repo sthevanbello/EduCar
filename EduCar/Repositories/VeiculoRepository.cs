ee27642a5bb241b424e05c27826146d7fdc11a78
Paloma Souza <paloma.souza112@gmail.com>
2022-10-10 09:45:39 -0300

    Controller de Veiculos adicionado

--- EduCar/Repositories/VeiculoRepository.cs
+++ EduCar/Repositories/VeiculoRepository.cs
@@ -1,6 +1,9 @@
 ﻿using EduCar.Contexts;
 using EduCar.Interfaces;
 using EduCar.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EduCar.Repositories
 {
@@ -10,8 +13,26 @@ namespace EduCar.Repositories
     /// </summary>
     public class VeiculoRepository : BaseRepository<Veiculo>, IVeiculoRepository
     {
+        private readonly EduCarContext _context;
         public VeiculoRepository(EduCarContext context) : base(context)
         {
+            _context = context;
+        }
+        
+
+        public Veiculo GetPlaca(string placa)
+        {
+            var veiculoPlaca = _context.Veiculo
+                  .Include(c => c.CaracteristicasGerais) // inclui a classe Medico para ser exibida
+                   .Include(f => f.FichaTecnica)  // inclui a classe Especialidade para ser exibida
+                  .FirstOrDefault(v => v.CaracteristicasGerais.Placa == placa); // quando o id do tipo de usuario for igual a 1 (que são os médicos)           
+
+            return veiculoPlaca;
+        }
+
+        public ICollection<Veiculo> GetStatusDisponivel()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
