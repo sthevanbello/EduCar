ea5dee9ad8803f7edfbd502f68e458d2e9bc1b3b
sthevanbello <sthevan.alves@gmail.com>
2022-10-10 12:56:19 -0300

    - Criados os controllers de Cambio e Direção  - Migration atualizada com StatusVenda e Cartão na model Pedido

---
+++ EduCar/Controllers/DirecoesController.cs
@@ -0,0 +1,53 @@
+﻿using EduCar.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EduCar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirecoesController : ControllerBase
+    {
+        private readonly IDirecaoRepository _direcaoRepository;
+
+        public DirecoesController(IDirecaoRepository direcaoRepository)
+        {
+            _direcaoRepository = direcaoRepository;
+        }
+        [HttpGet]
+        public IActionResult GetDirecoes()
+        {
+            try
+            {
+                var direcoes = _direcaoRepository.GetAll();
+
+                return Ok(direcoes);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Msg = "Falha ao exibir a lista de direções", ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByIdDirecao(int id)
+        {
+            try
+            {
+                var direcao = _direcaoRepository.GetById(id);
+                if (direcao == null)
+                {
+                    return NotFound(new { Msg = "Tipo de direção não encontrada" });
+                }
+                return Ok(direcao);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Msg = "Falha ao exibir tipo de direção", ex.Message });
+            }
+        }
+    }
+}
