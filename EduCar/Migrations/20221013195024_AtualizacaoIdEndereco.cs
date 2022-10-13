d34906424b3567be555aeaf306a98fb5363c99f0
sthevanbello <sthevan.alves@gmail.com>
2022-10-13 17:04:43 -0300

    - Atualização da migration  - Atualização do SolicitarTokenSenha - Colocado o e-mail remetente no appsettings.json

---
+++ EduCar/Migrations/20221013195024_AtualizacaoIdEndereco.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduCar.Migrations
+{
+    public partial class AtualizacaoIdEndereco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuario_Endereco_IdEndereço",
+                table: "Usuario");
+
+            migrationBuilder.RenameColumn(
+                name: "IdEndereço",
+                table: "Usuario",
+                newName: "IdEndereco");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Usuario_IdEndereço",
+                table: "Usuario",
+                newName: "IX_Usuario_IdEndereco");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuario_Endereco_IdEndereco",
+                table: "Usuario",
+                column: "IdEndereco",
+                principalTable: "Endereco",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuario_Endereco_IdEndereco",
+                table: "Usuario");
+
+            migrationBuilder.RenameColumn(
+                name: "IdEndereco",
+                table: "Usuario",
+                newName: "IdEndereço");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Usuario_IdEndereco",
+                table: "Usuario",
+                newName: "IX_Usuario_IdEndereço");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuario_Endereco_IdEndereço",
+                table: "Usuario",
+                column: "IdEndereço",
+                principalTable: "Endereco",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+    }
+}
